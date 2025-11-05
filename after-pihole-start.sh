7743286fe6a5dbef50bcc829453b9a1072ed9201
frankebobdk <50303454+frankebobdk@users.noreply.github.com>
2025-11-05 12:57:04 +0100

    Update after-pihole-start.sh

--- after-pihole-start.sh
+++ after-pihole-start.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-set -e
-/usr/local/bin/init-config.sh
-# Start Redis i baggrunden
+set -eu
+# log alt til egen fil (nem fejlsøgning)
+exec >>/var/log/after-hook.log 2>&1
+echo "[HOOK] $(date) starting"
+# giv Pi-hole et par sekunder til at komme i gang
+sleep 2
+# initialisering (idempotent)
+ /usr/local/bin/init-config.sh
+# start redis i baggrunden
 redis-server /config/redis/redis.conf &
-# Unbound i forgrund (beholder processen i live)
+echo "[HOOK] redis started (pid $!)"
+# start unbound i forgrunden i denne baggrunds-job
 exec unbound -d -c /config/unbound/unbound.conf
